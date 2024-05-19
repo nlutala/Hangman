7169f0556aa0ea3f3994e7d016c071449019f632
nlutala <nathanlutala@outlook.com>
2024-05-19 09:24:27 +0100

    Change the random_hangman_word.py file so that it only counts the minimum number of letters a word can have rather than the max limit to avoid NoWordsToChooseFrom error

--- random_hangman_word.py
+++ random_hangman_word.py
@@ -11,16 +11,13 @@ class RandomHangmanWord:
         '''
         self.level = level
         self.word_min_length = 0
-        self.word_max_length = 0
         
         if level == "EASY":
-            self.word_max_length = 4
-        elif level == "MEDIUM":
             self.word_min_length = 4
-            self.word_max_length = 7
-        else:
+        elif level == "MEDIUM":
             self.word_min_length = 7
-            self.word_max_length = 20
+        else:
+            self.word_min_length = 10
 
     def generateRandomWord(self, random_letter) -> str:
         '''
@@ -28,7 +25,6 @@ class RandomHangmanWord:
         '''
         return RandomWord().word(
             starts_with=random_letter,
-            word_min_length=self.word_min_length, 
-            word_max_length=self.word_max_length
+            word_min_length=self.word_min_length
         )
     
